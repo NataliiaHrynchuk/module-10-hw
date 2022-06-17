40a7fe1c32a776883d7221d51ea0e2da25345fdf
NataliiaHrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-06-17 13:22:34 +0000

    Deploying to gh-pages from @ NataliiaHrynchuk/module-10-hw@8d89ea95891f1c984dd96935c614ddffab863c6d 🚀

---
+++ index.68263bb8.js
@@ -0,0 +1,2 @@
+[document.querySelector("#search-box"),document.querySelector(".country-list"),document.querySelector(".country-info")][0].addEventListener("input",(function(e){var t=e.target.value;e.preventDefault(),t&&console.log(t)}));
+//# sourceMappingURL=index.68263bb8.js.map
