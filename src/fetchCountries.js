b010403d4ecc58ed6eb9da9703e146cd707cf0ac
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-06-22 23:04:05 +0300

    update

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,14 +1,19 @@
 const BASE_URL = 'https://restcountries.com/v3.1/name/'; 
+import Notiflix from 'notiflix';
 
 export const fetchCountries = ({ name }) => {
     console.log(name);
     
     return fetch(`${BASE_URL}${name}?fields=name,capital,population,flags,languages`)
         .then(response => {
+            if (!response.ok) {
+                throw new Error (response.statusText);
+            }
             return response.json();
         })
         
         .catch(error => {
-            console.log(error);
+            // console.log(error);
+            Notiflix.Notify.failure('Oops, there is no country with that name');
         });
     }
--- src/index.js
+++ src/index.js
@@ -7,11 +7,14 @@ const refs = {
     countryList: document.querySelector('.country-list'),
     countryInfo: document.querySelector('.country-info'),
 }
+
 let name = '';
 const onSearch = (event) => {
     const { value } = event.target;
     event.preventDefault();
-    
+    refs.countryList.innerHTML = "";
+    refs.countryInfo.innerHTML = "";
+    refs.countryList.classList.remove("big");
     if (!value) return;
     // console.log(value);
     name = value.trim();
@@ -30,19 +33,47 @@ const onSearch = (event) => {
                         name: { official },
                     
                     } = country;
-                console.log(svg, official)});
-                
+                console.log(svg, official);
+                refs.countryList.insertAdjacentHTML("beforeend", renderCountriesList(svg, official));
+            });
             }
             if (countries.length === 1) {
-                countries.map(country => console.log(country.name.official, country.capital, country.population, country.flags.svg, country.languages));
+                
+                countries.map((country) => {
+                    const {
+                        flags: { svg },
+                        name: { official },
+                        capital,
+                        population,
+                        languages
+                    
+                    } = country;
+                    console.log(official, capital, population, svg, languages);
+                    refs.countryList.innerHTML = renderCountriesList(svg, official);
+                    refs.countryList.classList.add("big");
+                    refs.countryInfo.innerHTML = renderCountriesInfo(capital, population, languages);
+                });
             }
         });
         
     
 }
 
-const render = (name) => {
-    //render new list here
-    console.log(name);
+const renderCountriesList = (svg, official) => {
+   return `<li>
+   <div class="country-name">
+     <img src="${svg}" alt="flag" width="100">
+     <h2 class="country-title">${official}</h2>
+   </div>
+ </li>`;
 };
+
+function renderCountriesInfo(capital, population, languages )  {
+    const languagesName = Object.values(languages).join(', ');
+    console.log(languagesName);
+    
+    return `<p class="country-text"><span>Capital:</span>${capital}</p>
+            <p class="country-text"><span>Population:</span>${population}</p>
+            <p class="country-text"><span>Languages:</span>${languagesName}</p>`
+}
 refs.input.addEventListener('input', _.debounce(onSearch, DEBOUNCE_DELAY));
